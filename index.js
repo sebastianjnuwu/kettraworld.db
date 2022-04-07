7a6ee0c67f122ffdef91b26bb24b710353d1a826
SEBASTIAN JN ฅ^•ﻌ•^ฅ <101356556+sebastianjn@users.noreply.github.com>
2022-04-07 12:02:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const JsonDatabase = require("./src/Json");
-const Dotenv = require("./src/Env")
-const mysql = require("./src/index.js")
+const Dotenv = require("./src/Env");
+const mysql = require("./src/index.js");
+const fs = require("fs");
 
-module.exports = { JsonDatabase, Dotenv, mysql };
+module.exports = { JsonDatabase, Dotenv, mysql, fs };
