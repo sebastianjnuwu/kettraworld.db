790d97908beeed2fa956e5b27c4058e2ce129d55
SEBASTIAN JN ฅ^•ﻌ•^ฅ <101356556+sebastianjn@users.noreply.github.com>
2022-04-07 20:27:35 -0300

    update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const JsonDatabase = require("./src/Json");
-const Dotenv = require("./src/Env");
+const JsonDatabase = require("./src/Json.js");
+const Dotenv = require("./src/Env.js");
 const mysql = require("./src/index.js");
 const base64 = require("./src/base64.js");
 const utf8 = require("./src/utf8.js");
