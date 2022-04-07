5fc8b8dadecbc01f646281be9ed4a8c445ebb4e8
SEBASTIAN JN ฅ^•ﻌ•^ฅ <101356556+sebastianjn@users.noreply.github.com>
2022-04-07 12:05:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const JsonDatabase = require("./src/Json");
 const Dotenv = require("./src/Env");
 const mysql = require("./src/index.js");
 const fs = require("fs");
+const colors = require("colors");
 
-module.exports = { JsonDatabase, Dotenv, mysql, fs };
+module.exports = { JsonDatabase, Dotenv, mysql, fs, colors };
